649549ef4431e47a687c8b9be9f2c4fa662737ab
Manuel Youssef Morales Matta <mmorales@dell1425.tbn.com.mx>
2022-08-19 15:09:30 -0500

    Checkout Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 const Cart = () => {
 
-    return (<>
+    const [products, setProducts] = useState([])
+    const getProducts = async () => {
+        const url = 'https://proyecto5-backend.herokuapp.com/api/v1/products'
+        const response = await axios.get(url)
+        // axios.get(url)
+        //   .then((res) => console.log(res))
+        console.log("Hola")
+        console.log(response)
+        setProducts(response.data)
+    }
 
-        <div class="content">
-            <h2>Cart</h2>
+    useEffect(() => {
+        getProducts()
+    }, [])
+    return (<>
+        <div className=" container">
+            <h3 className="card-title">Checkout</h3><hr />
+            {products.map((product, index) =>
+                <div key={index}>
+                    <p>{product.title}</p><hr />
+                </div>
+            )} <hr />
+            {products.length ?
+                <div><h4><small>Total Amount:</small><span className="float-right text-primary">
+                   </span></h4><hr /></div> : ''}
+            {!products.length ? <h3 className="text-warning">No item on the cart</h3> : ''}
+            {products.length ? <button className="btn btn-success float-right">Pay</button> : ''}
+            <Link to="/"><button className="btn btn-danger float-right"
+                style={{ marginRight: "10px" }}>Cancel</button></Link><br /><br /><br />
         </div>
     </>)
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,47 @@
 
-import ProfileUi from 'react-profile-card';
 const Profile = () => {
 
-    return (
-        <div> 
-        <ProfileUi 
-            imgUrl='../../../src/imgs/GP.png'
-            name='Youssef Matta' 
-            designation='Developer' 
-            />
-      </div>
-	);
+    return (<>
+        <div class="container my-4">
+            <form class="col-md-12 mb-12">
+                <div class="col text-center">
+                    <div class="col-md-12 mb-12">
+                        <img class="rounded-circle" alt="100x100" src="https://freesvg.org/img/abstract-user-flat-4.png"
+                            data-holder-rendered="true" style={{ width: '300px', height: '300px' }} />
+                    </div>
+                    <div class="col text-center">
+                        <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label">Nombre Completo</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" id="nombre" disabled="true" placeholder="Nombre Completo" value="General de Productos SA de CV" />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Rol</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" id="nombre" disabled="true" placeholder="Nombre Completo" value="Administrador" />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" id="nombre" disabled="true" placeholder="Nombre Completo" value="generalproductos@gmail.com" />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-3">
+                                <input type="password" class="form-control" id="nombre" disabled="true" placeholder="Nombre Completo" value="12345" />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+
+        </div>
+    </>
+    );
 
 }
 
